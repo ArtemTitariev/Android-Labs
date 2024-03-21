6c5df513102db628a1346a887d75a0b35f61e9f3
Artem Titariev <artem.titariev@gmail.com>
2024-03-21 14:17:32 +0200

    [lr8] Add data view in MainActivity

--- LR8/app/src/main/java/com/example/lr8/MainActivity.kt
+++ LR8/app/src/main/java/com/example/lr8/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.example.lr8
 
-import androidx.appcompat.app.AppCompatActivity
+import DatabaseHelper
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lr8.databinding.ActivityMainBinding
+import com.example.lr8.models.Artwork
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var db: SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.npYear.apply {
+            setMaxValue(2100);
+            setMinValue(1900)
+        }
+
+
+        val dbHelper = DatabaseHelper(this)
+        db = dbHelper.writableDatabase
+
+
+        val adapter = ArtworkAdapter(dbHelper.getArtworkList())
+        binding.rvArtworks.adapter = adapter
+        binding.rvArtworks.layoutManager = LinearLayoutManager(this)
     }
+
+
 }
